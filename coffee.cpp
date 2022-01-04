e2aabb06fcb790779f6d5436452db0c33b73b8ab
Osef Alfan Fadhil <70691740+osefdb@users.noreply.github.com>
2022-01-05 01:38:03 +0700

    Update coffee.cpp

--- coffee.cpp
+++ coffee.cpp
@@ -1,4 +1,4 @@
-//Libraries
+//Import Libraries
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
@@ -12,18 +12,23 @@ int motor[]={6,7,8,9};
 
 //Variable Declaration
 String flavour[]={"Latte","Cappucino","Espresso","Mocha"};
-int price[]={5,5,8,8};
+int sen_pin=A0;
+int t_c;
+float t_f;
+float v_t;
 
 //Object Declaration
-LiquidCrystal_I2C lcd(0x20,20,4);
+LiquidCrystal_I2C lcd(0x20,20,4); //size display
 
 //Setup Function
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   for(int i=0;i<4;i++){
-    pinMode(button[i],INPUT_PULLUP);
-    pinMode(motor[i],OUTPUT);
+    pinMode(button[i],INPUT_PULLUP); //input button
+    pinMode(motor[i],OUTPUT); //output motor
+    pinMode(sen_pin, INPUT); //input sensor lm35
+    pinMode(0,OUTPUT); //output heater
   }
   pinMode(water,OUTPUT);
   pinMode(sugar,OUTPUT);
@@ -33,38 +38,50 @@ void setup() {
   //Print a message to the LCD
   lcd.backlight();
   lcd.setCursor(0,0);
-  lcd.print("DO");
+  lcd.print("Kelompok DOM");
   lcd.setCursor(0,1);
-  lcd.print("Projects");
+  lcd.print("Projects UAS");
   lcd.setCursor(0,2);
-  lcd.print("Smart Coffee");
+  lcd.print("Mesin Kopi");
   lcd.setCursor(0,3);
-  lcd.print("Machine");
+  lcd.print("Otomatis");
   delay(1000);
   lcd.clear();
 }
 
 //Loop Function
 void loop() {
+  v_t=analogRead(sen_pin);
+  v_t=(v_t*500)/1023; //konversi voltage to temperature
+  t_c=v_t;
+  t_f=(v_t*1.8)+32; //konversi celcius to fahrenheit
+  Serial.println(" Deg_c=");
+  Serial.println(t_c);
+  delay(500);
   // put your main code here, to run repeatedly:
-  for(int i=0;i<4;i++){
-    lcd.setCursor(0,i);
-    lcd.print(i+1);
-    lcd.print(" for ");
-    lcd.print(flavour[i]);
-    lcd.setCursor(17,i);
-    lcd.print(price[i]);
-    lcd.print("/-");
+  for(int j=0;j<4;j++){
+    lcd.setCursor(0,j);
+    lcd.print(j+1);
+    lcd.print(" untuk ");
+    lcd.print(flavour[j]);
   }
-  for(int i=0;i<4;i++){
-    if(digitalRead(button[i])==LOW){
+  for(int k=0;k<4;k++){
+    if(digitalRead(button[k])==LOW){
+      if(t_c>=80){
+        digitalWrite(0,LOW);
+        }
+      if(t_c<80 && t_c>=0){
+        digitalWrite(0,HIGH);
+        }
+        delay(5000); //manasin air
+        digitalWrite(0,LOW);
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("Wait");
+      lcd.print("Mohon Tunggu");
       lcd.setCursor(0,1);
-      lcd.print("Collecting");
+      lcd.print("Sedang Mengumpulkan");
       lcd.setCursor(0,2);
-      lcd.print("Ingredients");
+      lcd.print("Bahan-bahan");
       delay(1000);
       digitalWrite(water, HIGH);
       delay(2000);
@@ -72,36 +89,33 @@ void loop() {
       digitalWrite(sugar, HIGH);
       delay(2000);
       digitalWrite(sugar, LOW);
-      digitalWrite(coffee, LOW);
-      digitalWrite(motor[i], HIGH);
+      digitalWrite(coffee, HIGH);
+      digitalWrite(motor[k], HIGH);
       delay(2000);
-      digitalWrite(motor[i], LOW);
+      digitalWrite(coffee, LOW);
+      digitalWrite(motor[k], LOW);
       lcd.clear();
       delay(1000);
       lcd.setCursor(0,0);
-      lcd.print("Wait");
+      lcd.print("Mohon tunggu");
       lcd.setCursor(0,1);
-      lcd.print("Yours Rich");
+      lcd.print("Kopi ");
+      lcd.print(flavour[k]);
       lcd.setCursor(0,2);
-      lcd.print(flavour[i]);
-      lcd.print("is");
+      lcd.print("anda sedang");
       lcd.setCursor(0,3);
-      lcd.print("gettin ready");
+      lcd.print("disiapkan.");
       digitalWrite(mixer, HIGH);
       delay(10000);
       digitalWrite(mixer, LOW);
       lcd.clear();
       lcd.setCursor(0,0);
-      lcd.print("Your richs");
+      lcd.print("Kopi ");
+      lcd.print(flavour[k]);
       lcd.setCursor(0,1);
-      lcd.print(flavour[i]);
-      lcd.print("is ready");
+      lcd.print("anda telah siap.");
       lcd.setCursor(0,2);
-      lcd.print("collect it");
-      lcd.setCursor(0,3);
-      lcd.print("yours amount");
-      lcd.print(price[i]);
-      lcd.print("/-");
+      lcd.print("Silahkan menikmati!");
       delay(5000);
       lcd.clear();
     }
